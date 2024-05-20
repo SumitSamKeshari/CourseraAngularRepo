778c73c7f0e3ffaa486a508b5e52bf59d9a62dd7
Sumit Kumar <SumitSamkeshri@gmail.com>
2024-05-20 21:22:31 +0530

    Lecture12 to Lecture17

---
+++ Example/Lecture12/app.js
@@ -0,0 +1,20 @@
+(function (){
+  'use strict';
+  angular.module('MsgApp',[])
+  .controller('MsgController',MsgController);
+
+  MsgController.$inject=['$scope'];
+  function MsgController($scope){
+    $scope.name ="Sumit";
+    $scope.stateOfBeing ="Short";
+    $scope.cookieCost=.45;
+
+    $scope.sayMassage=function(){
+      return "Sumit likes to eat healthy snacks at night!"
+    };
+
+    $scope.feedSourabh=function() {
+      $scope.stateOfBeing ="Long";
+    }
+  }
+})();
---
+++ Example/Lecture13_Part3/app.js
@@ -0,0 +1,46 @@
+(function (){
+  'use strict';
+  angular.module('MsgApp',[])
+  .controller('MsgController',MsgController)
+  .filter('loves',LovesFilter)
+  .filter('truth',TruthFilter);
+
+  MsgController.$inject=['$scope',lovesFilter];
+  function MsgController($scope,lovesFilter){
+    $scope.name ="Sumit";
+    $scope.stateOfBeing ="Short";
+
+    $scope.sayMassage=function(){
+      return "Sumit likes to eat healthy snacks at night!"
+    };
+
+    $scope.sayLovesMassage=function(){
+      var msg= "Sumit likes to eat healthy snacks at night!"
+      msg = lovesFilter(msg);
+      return msg;
+    };
+
+    $scope.feedSourabh=function() {
+      $scope.stateOfBeing ="Long";
+    };
+  }
+
+function LovesFilter(){
+  return function( input) {
+    input = input || "";
+    input =input.replace("likes","loves");
+    return  input;
+  };
+}
+
+
+function TruthFilter (){
+  return function ( input, target, replace){
+    input = input || "";
+    input =input.replace(target,replace);
+    return  input;
+  }
+}
+
+
+})();
